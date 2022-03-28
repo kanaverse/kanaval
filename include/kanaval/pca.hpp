8c3c97ad40d7787b0c4f655a2f454d94cfb3580c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-03-28 10:20:21 -0700

    Support corrected PCs, specification of blocking method.

--- include/kanaval/pca.hpp
+++ include/kanaval/pca.hpp
@@ -18,7 +18,7 @@ namespace pca {
 /**
  * @cond
  */
-inline int validate_parameters(const H5::Group& handle) {
+inline std::pair<int, std::string> validate_parameters(const H5::Group& handle) {
     auto phandle = utils::check_and_open_group(handle, "parameters");
 
     auto nhvgs = utils::load_integer_scalar<>(phandle, "num_hvgs");
@@ -31,10 +31,15 @@ inline int validate_parameters(const H5::Group& handle) {
         throw std::runtime_error("number of PCs must be positive in 'num_pcs'");
     }
 
-    return npcs;
+    auto method = utils::load_string(phandle, "block_method");
+    if (method != "none" && method != "regress" && method != "mnn") {
+        throw std::runtime_error("unrecognized value '" + method + "' for the 'block_method'");
+    }
+
+    return std::make_pair(npcs, method);
 }
 
-inline void validate_results(const H5::Group& handle, int num_pcs, int num_cells) {
+inline void validate_results(const H5::Group& handle, int num_pcs, std::string block_method, int num_cells) {
     auto rhandle = utils::check_and_open_group(handle, "results");
 
     std::vector<size_t> pdims { static_cast<size_t>(num_cells), static_cast<size_t>(num_pcs) };
@@ -42,6 +47,10 @@ inline void validate_results(const H5::Group& handle, int num_pcs, int num_cells
 
     std::vector<size_t> vdims { static_cast<size_t>(num_pcs) };
     utils::check_and_open_dataset(rhandle, "var_exp", H5T_FLOAT, vdims);
+
+    if (block_method == "mnn") {
+        utils::check_and_open_dataset(rhandle, "corrected", H5T_FLOAT, pdims);
+    }
 
     return;
 }
@@ -58,13 +67,21 @@ inline void validate_results(const H5::Group& handle, int num_pcs, int num_cells
  *
  * - `num_hvgs`: a scalar integer containing the number of highly variable genes to use to compute the PCA.
  * - `num_pcs`: a scalar integer containing the number of PCs to compute.
+ * - `block_method`: a scalar string specifying the method to use when dealing with multiple blocks in the dataset.
+ *   This may be `"none"`, `"regress"` or `"mnn"`.
  *
  * `results` should contain:
  *
  * - `pcs`: a 2-dimensional float dataset containing the PC coordinates in a row-major layout.
  *   Each row corresponds to a cell (after QC filtering) and each column corresponds to a PC.
  *   Note that this is deliberately transposed from the Javascript/Wasm representation for easier storage.
+ *   - If `block_type = "mnn"`, the PCs will be computed using a weighted method that adjusts for differences in the number of cells across blocks.
+ *   - If `block_type = "regress"`, the PCs will be computed on the residuals after regressing out the block-wise effects.
  * - `var_exp`: a float dataset of length equal to the number of PCs, containing the percentage of variance explained by each PC.
+ *
+ * If `block_type = "mnn"`, the `results` group will also contain:
+ *
+ * - `corrected`, a float dataset with the same dimensions as `pcs`, containing the MNN-corrected PCs for each cell.
  *
  * @param handle An open HDF5 file handle.
  * @param num_cells Number of cells in the dataset after any quality filtering is applied.
@@ -76,14 +93,17 @@ inline void validate(const H5::H5File& handle, int num_cells) {
     auto phandle = utils::check_and_open_group(handle, "pca");
 
     int npcs;
+    std::string bmethod;
     try {
-        npcs = validate_parameters(phandle);
+        auto pout = validate_parameters(phandle);
+        npcs = pout.first;
+        bmethod = pout.second;
     } catch (std::exception& e) {
         throw utils::combine_errors(e, "failed to retrieve parameters from 'pca'");
     }
 
     try {
-        validate_results(phandle, npcs, num_cells);
+        validate_results(phandle, npcs, bmethod, num_cells);
     } catch (std::exception& e) {
         throw utils::combine_errors(e, "failed to retrieve results from 'pca'");
     }
